69aba8dbab8a3ec450db245902fdbe033cea2f2f
moath haytham <moath-moh@hotmail.com>
2018-04-20 23:57:32 +0200

    final code isa ya3ny

--- src/scanner/gui.java
+++ src/scanner/gui.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package sacnner;
+package scanner;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
@@ -38,74 +38,43 @@ public class gui extends JFrame{
     private JPanel pnlButtons = new JPanel();
     private JPanel pnlcenter = new JPanel();
     private JPanel pnlheader = new JPanel();
-    /*int width = (int) screenSize.getWidth();
-    int height = (int) screenSize.getHeight();*/
+
     int w = (int)(screenSize.getWidth())/2;
     int h  = (int) (screenSize.getHeight())/2;
+    
     String in = null;
     void init(){
         
         create();
         setSize(500, 500);
         this.setLocation(screenSize.width/2-this.getSize().width/2, screenSize.height/2-this.getSize().height/2);
-        //setSize(500, 500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true); 
     }
     void create(){
-    //this.setLayout(null);
+    
     this.add(pnlheader,BorderLayout.NORTH);
-        this.add(pnlcenter);
+    this.add(pnlcenter);
     this.add(pnlButtons,BorderLayout.SOUTH);
     
     label = new JLabel("How do you want to enter your input?");
-    //label.setSize(60,60);
-    //label.setLocation(w/4, h/8);
     pnlheader.add(label, BorderLayout.CENTER);
     
     labelIn = new JLabel("Input:");
-    //labelIn.setSize(60,60);
-    //labelIn.setLocation(w/4, (h/8)-25);
     labelIn.setVisible(false);
     
     labelout = new JLabel("Output:");
-    //labelout.setSize(200,200);
-    //labelout.setLocation(w/2, (h/8)-25);
     labelout.setVisible(false);
     
     input = new JTextArea();
-    //input.setSize(100,100);
-    //input.setLocation(w/4,  (h/8)+30);
     input.setVisible(false);
     
     output = new JTextArea();
-    /*output.setSize(w/4,h/4);
-    output.setLocation(w/2,  (h/8)+30);*/
     output.setVisible(false);
     output.setEditable(false);
     
-    /*pnlcenter.setLayout(new GridBagLayout());
-    gbc.weightx = 1.0;
-    //gbc.weighty = 1.0;
-    gbc.anchor = GridBagConstraints.CENTER;
-    gbc.fill = GridBagConstraints.BOTH;
-    //gbc.ipady = 900;
-    gbc.insets=new Insets(0,0,0,100);
-    gbc.gridy=0;
-    //pnlcenter.setSize(200, 200);
-    pnlcenter.add(labelIn,gbc);
-    gbc.insets=new Insets(0,100,0,0);
-    pnlcenter.add(labelout,gbc);
-    gbc.gridy=1;
-    gbc.insets=new Insets(0,0,0,100);
-    pnlcenter.add(input,gbc);
-    gbc.insets=new Insets(0,100,0,0);
-    pnlcenter.add(output,gbc);*/
-    
-    
     txtIn = new JButton("Read Code from Text File");
     txtIn.setSize(100,220);
-    //txtIn.setLocation(w/7, h/3);
     
     ManualIn= new JButton("Enter Code Manually");
     ManualIn.setSize(100,100);
@@ -114,19 +83,14 @@ public class gui extends JFrame{
     gbc.weightx = 0.0;
     gbc.gridwidth = 1;
     
-    //ManualIn.setLocation(w/2, h/3);
     pnlcenter.setLayout(new GridBagLayout());
     gbc.insets=new Insets(0,0,0,50);
     pnlcenter.add(txtIn,gbc);
     gbc.insets=new Insets(0,50,0,0);
     pnlcenter.add(ManualIn,gbc);
     
-    
-    
     pnlButtons.setLayout(new GridBagLayout());
     enter = new JButton("Enter");
-    //enter.setSize(w/7,h/8);
-    //enter.setLocation((w/2)+60,(int)(h/1.8)+100);
     enter.setVisible(false);
     pnlButtons.add(enter);
     
@@ -138,13 +102,10 @@ public class gui extends JFrame{
         pnlcenter.removeAll();
         pnlcenter.setLayout(new GridBagLayout());
         gbc.weightx = 1.0;
-        //gbc.weighty = 1.0;
         gbc.anchor = GridBagConstraints.CENTER;
         gbc.fill = GridBagConstraints.BOTH;
-        //gbc.ipady = 60;
         gbc.insets=new Insets(0,10,0,100);
         gbc.gridy=0;
-        //pnlcenter.setSize(200, 200);
         pnlcenter.add(labelIn,gbc);
         gbc.insets=new Insets(0,100,0,0);
         pnlcenter.add(labelout,gbc);
@@ -167,9 +128,8 @@ public class gui extends JFrame{
         @Override
         public void actionPerformed(ActionEvent e) {
             setSize(1000, 1000);
-            
             label.setText("Enter your code in the input text area provided");
-           input.setVisible(true);
+            input.setVisible(true);
             visble();
         }
         
@@ -190,52 +150,27 @@ public class gui extends JFrame{
     txtIn.addActionListener(new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
-
-            /*output.setLocation(w/4,  (h/8)+30);
-            output.setSize(w/2,h/2);
-            labelIn.setVisible(false);
-            add(output);
-            txtIn.setVisible(false);
-            pnlcenter.removeAll();
-            pnlcenter.setLayout(new GridBagLayout());            
-            label.setText("Your code has been imported and scanned");
-            gbc.weightx = 1.0;
-            //gbc.weighty = 1.0;
-            gbc.anchor = GridBagConstraints.CENTER;
-            gbc.fill = GridBagConstraints.BOTH;
-            //gbc.ipady = 60;
-            gbc.insets=new Insets(0,0,0,100);
-            gbc.gridy=0;
-            pnlcenter.add(labelout,gbc);
-            gbc.gridy=1;
-            pnlcenter.add(output,gbc);
-             //visble();*/
-
             try {
                 setSize(1000, 1000);
+                
                 txtIn.setVisible(false);
                 ManualIn.setVisible(false);
+                
                 label.setText("Your code has been imported and scanned");
-            pnlcenter.removeAll();
-            pnlcenter.setLayout(new GridBagLayout());            
-            gbc.weightx = 1.0;
-            //gbc.weighty = 1.0;
-            gbc.anchor = GridBagConstraints.CENTER;
-            gbc.fill = GridBagConstraints.BOTH;
-            //gbc.ipady = 60;
-            gbc.insets=new Insets(0,0,0,100);
-            gbc.gridy=0;
-            pnlcenter.add(labelout,gbc);
-            gbc.gridy=1;
-            pnlcenter.add(output,gbc);
-            output.setVisible(true);
-            labelout.setVisible(true);
-             //visble();
-                //output.setLocation(w/4,  (h/8)+30);
-                //output.setSize(w/2,h/2);
-                //labelIn.setVisible(false);
-                //add(output);
-                //visble();
+                
+                pnlcenter.removeAll();
+                pnlcenter.setLayout(new GridBagLayout());            
+                
+                gbc.weightx = 1.0;
+                gbc.anchor = GridBagConstraints.CENTER;
+                gbc.fill = GridBagConstraints.BOTH;
+                gbc.insets=new Insets(0,0,0,100);   
+                gbc.gridy=0;
+                pnlcenter.add(labelout,gbc);
+                gbc.gridy=1;
+                pnlcenter.add(output,gbc);
+                output.setVisible(true);
+                labelout.setVisible(true);
                 output.setText(Scanner.scan(""));
 
             } catch (IOException ex) {
